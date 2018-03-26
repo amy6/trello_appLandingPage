862b2dde35048af63df6a0abdcc33a3b881455c3
mahima <mahima.6.ms@gmail.com>
2018-03-26 12:27:25 +0530

    set onClickListener for Taco image animation and playstore/appstore buttons dynamically

--- app/src/main/java/com/example/android/applandingpage/ImageSliderAdapter.java
+++ app/src/main/java/com/example/android/applandingpage/ImageSliderAdapter.java
@@ -1,12 +1,19 @@
 package com.example.android.applandingpage;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ScrollView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.ArrayList;
 
@@ -33,17 +40,68 @@ public class ImageSliderAdapter extends PagerAdapter {
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         ArrayList<View> viewArrayList = new ArrayList<>();
 
         for (int layout : layouts) {
-            View view = null;
+
             if (inflater != null) {
-                view = inflater.inflate(layout, container, false);
+                View view = inflater.inflate(layout, container, false);
+                viewArrayList.add(view);
+                container.addView(view);
+
+                switch (position) {
+                    case 2:
+                        final ImageView imageView = view.findViewById(R.id.trello_taco_img);
+                        if (imageView != null) {
+                            imageView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    YoYo.with(Techniques.StandUp)
+                                            .duration(2000)
+                                            .playOn(imageView);
+                                }
+                            });
+                        }
+                        break;
+                    case 3:
+                        ImageView playStoreImg = view.findViewById(R.id.playstore);
+                        ImageView appStoreImg = view.findViewById(R.id.appstore);
+                        final Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                        if(playStoreImg != null) {
+                            playStoreImg.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    try {
+                                        context.startActivity(intent.setData(Uri.parse("market://details?id=" +
+                                                context.getString(R.string.playstore_ID))));
+                                    } catch (ActivityNotFoundException exception) {
+                                        context.startActivity(intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" +
+                                                context.getString(R.string.playstore_ID))));
+                                    }
+                                }
+                            });
+                        }
+
+                        if(appStoreImg != null) {
+                            appStoreImg.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    try {
+                                        context.startActivity(intent.setData(Uri.parse("itms-apps://itunes.apple.com/app/id" +
+                                                context.getString(R.string.applestore_ID))));
+                                    } catch (ActivityNotFoundException exception) {
+                                        context.startActivity(intent.setData(Uri.parse("https://itunes.apple.com/app/id" +
+                                                context.getString(R.string.applestore_ID))));
+                                    }
+                                }
+                            });
+                        }
+                        break;
+                }
             }
-            viewArrayList.add(view);
-            container.addView(view);
         }
 
         return viewArrayList.get(position);
--- app/src/main/java/com/example/android/applandingpage/MainActivity.java
+++ app/src/main/java/com/example/android/applandingpage/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.android.applandingpage;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         ViewPager viewPager = findViewById(R.id.photos_viewpager);
         TabLayout tabLayout = findViewById(R.id.tabLayout);
@@ -29,46 +22,5 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setPageTransformer(true, new DepthPageTransformer());
         tabLayout.setupWithViewPager(viewPager);
 
-    }
-
-    /**
-     * Uses YoYo animation library to animate Image
-     * @param view Taco image reference
-     */
-    public void tacoClick(View view) {
-        YoYo.with(Techniques.StandUp)
-                .duration(2000)
-                .playOn(view);
-    }
-
-
-    /**
-     * Called when the app store buttons are clicked to open respective app download pages
-     * @param view button clicked
-     */
-    public void getTheApp(View view) {
-        int id = view.getId();
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-
-        switch (id) {
-            case R.id.playstore:
-                try{
-                    startActivity(intent.setData(Uri.parse("market://details?id=" + getString(R.string.playstore_ID))));
-                }
-                catch (ActivityNotFoundException exception) {
-                    startActivity(intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" + getString(R.string.playstore_ID))));
-                }
-                break;
-
-            case R.id.appstore:
-                try{
-                    startActivity(intent.setData(Uri.parse("itms-apps://itunes.apple.com/app/id" + getString(R.string.applestore_ID))));
-                }
-                catch (ActivityNotFoundException exception) {
-                    startActivity(intent.setData(Uri.parse("https://itunes.apple.com/app/id" + getString(R.string.applestore_ID))));
-                }
-
-                break;
-        }
     }
 }
