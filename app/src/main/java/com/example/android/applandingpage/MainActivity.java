6d2e0803de626f0bcaa08c81db1a273461899ba2
mahima <mahima.6.ms@gmail.com>
2018-03-26 19:23:22 +0530

    add commented code for changing page indicator color for each page dynamically

--- app/src/main/java/com/example/android/applandingpage/MainActivity.java
+++ app/src/main/java/com/example/android/applandingpage/MainActivity.java
@@ -8,19 +8,78 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+//    private LinearLayout pageIndicatorLayout;
+//    private int numOfPages;
+//    private int[] colors = {R.color.colorOne, R.color.colorTwo, R.color.colorThree, R.color.colorFour};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.photos_viewpager);
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager = findViewById(R.id.photos_viewpager);
+        tabLayout = findViewById(R.id.tabLayout);
 
         ImageSliderAdapter adapter = new ImageSliderAdapter(this);
         viewPager.setAdapter(adapter);
         viewPager.setPageTransformer(true, new DepthPageTransformer());
         tabLayout.setupWithViewPager(viewPager);
 
+        /*numOfPages = adapter.getCount();
+        pageIndicatorLayout = findViewById(R.id.dotsIndicatorLayout);
+
+        addPageIndicator(0);*/
+
+//        viewPager.addOnPageChangeListener(pageChangeListener);
+
     }
+
+    /*ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addPageIndicator(position);
+
+//            View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(position);
+//            tab.setBackgroundResource(colors[position]);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+
+      Set different color for each page indicator
+
+    private void addPageIndicator(int position) {
+
+        TextView[] indicators = new TextView[numOfPages];
+        pageIndicatorLayout.removeAllViews();
+
+        for(int i=0; i<indicators.length; i++) {
+
+            indicators[i] = new TextView(this);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(2,2,2,2);
+            indicators[i].setLayoutParams(layoutParams);
+            indicators[i].setText(Html.fromHtml("&#8226;"));
+            indicators[i].setTextSize(40);
+            indicators[i].setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+            pageIndicatorLayout.addView(indicators[i]);
+        }
+
+        if(indicators.length > 0) {
+            indicators[position].setTextColor(ContextCompat.getColor(this, colors[position]));
+        }
+    }*/
 }
